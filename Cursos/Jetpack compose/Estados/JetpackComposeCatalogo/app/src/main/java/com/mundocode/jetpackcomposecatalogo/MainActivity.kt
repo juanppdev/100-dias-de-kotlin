ac9fa532e9bdc0ce11bf13b383072d97ba6116fb
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-04-10 16:09:07 +0200

    State Hoisting en Jetpack Compose

--- Cursos/Jetpack compose/Estados/JetpackComposeCatalogo/app/src/main/java/com/mundocode/jetpackcomposecatalogo/MainActivity.kt
+++ Cursos/Jetpack compose/Estados/JetpackComposeCatalogo/app/src/main/java/com/mundocode/jetpackcomposecatalogo/MainActivity.kt
@@ -14,23 +14,34 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mundocode.jetpackcomposecatalogo.ui.theme.JetpackComposeCatalogoTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,20 +54,112 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyStateExample()
+                    var myText by remember { mutableStateOf("") }
+                    MyTextFieldState(myText) { myText = it }
                 }
             }
         }
     }
 }
 
+@Composable
+fun MyTextFieldState(name: String, onValueChange: (String) -> Unit) {
+    TextField(value = name, onValueChange = onValueChange)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextFieldOutline() {
+
+    var myText by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = myText,
+        onValueChange = { myText = it },
+        modifier = Modifier.padding(24.dp),
+        label = { Text(text = "Introduce tu Nombre") },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Magenta,
+            unfocusedBorderColor = Color.Blue
+        )
+    )
+
+}
+
+@Composable
+fun MyTextFieldAdvance() {
+
+    var myText by remember { mutableStateOf("") }
+
+    /*
+        TextField(
+        value = myText,
+        onValueChange = { myText = it },
+        label = { Text(text = "Introduce tu Nombre") })
+    */
+
+    TextField(
+        value = myText,
+        onValueChange = {
+            myText =
+                if (it.contains("a")) {
+                    it.replace("a", "")
+                } else {
+                    it
+                }
+        },
+        label = { Text(text = "Introduce tu Nombre") })
+
+}
+
+@Composable
+fun MyTextField() {
+    var myText by remember { mutableStateOf("") }
+    TextField(value = myText, onValueChange = { myText = it })
+}
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Este es un ejemplo")
+        Text(text = "Esto es un eejemplo", color = Color.Blue)
+        Text(text = "Esto es un eejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un eejemplo", fontWeight = FontWeight.Light)
+        Text(text = "Esto es un eejemplo", style = TextStyle(fontFamily = FontFamily.Cursive))
+        Text(text = "Esto es un eejemplo", fontFamily = FontFamily.Cursive)
+        Text(
+            text = "Esto es un eejemplo",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(text = "Esto es un eejemplo", textDecoration = TextDecoration.LineThrough)
+        Text(
+            text = "Esto es un eejemplo",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "Esto es un eejemplo",
+            style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(
+                        TextDecoration.Underline,
+                        TextDecoration.LineThrough
+                    )
+                )
+            )
+        )
+        Text(text = "Este es un ejemplo", fontSize = 30.sp)
+    }
+}
 
 @Composable
 fun MyStateExample() {
-    
-    var counter by rememberSaveable {mutableStateOf(0)}
 
-    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+    var counter by rememberSaveable { mutableStateOf(0) }
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Button(onClick = { counter += 1 }) {
             Text(text = "Pulsar")
         }
@@ -73,14 +176,15 @@ fun MyComplexLayout() {
                 .weight(1f)
                 .background(Color.Cyan),
             contentAlignment = Alignment.Center
-            ) {
+        ) {
             Text(text = "Ejemplo 1")
         }
         MySpcer(size = 30)
         Row(
             Modifier
                 .fillMaxWidth()
-                .weight(1f)) {
+                .weight(1f)
+        ) {
             Box(
                 Modifier
                     .weight(1f)
@@ -106,7 +210,7 @@ fun MyComplexLayout() {
                 .weight(1f)
                 .background(Color.Magenta),
             contentAlignment = Alignment.BottomCenter
-            ) {
+        ) {
             Text(text = "Ejemplo 4")
         }
     }
@@ -195,6 +299,7 @@ fun MyBox() {
 @Composable
 fun GreetingPreview() {
     JetpackComposeCatalogoTheme {
-        MyStateExample()
+        var myeTxt by remember { mutableStateOf("") }
+        MyTextFieldState(myeTxt) { myeTxt = it }
     }
 }
