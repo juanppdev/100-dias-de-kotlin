8dbce266c7e1dd0f0fcb0bac4caf47693570fdb3
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-04-13 15:23:52 +0200

    ProgressBar en Jetpack Compose

---
+++ Cursos/Jetpack compose/Componentes/ProgressBar/MyProgessBar.kt
@@ -0,0 +1,46 @@
+package com.mundocode.jetpackcomposecatalogo
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyProgessBar() {
+    var showLoading by rememberSaveable {
+        mutableStateOf(false)
+    }
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (showLoading) {
+            CircularProgressIndicator(color = Color.Red, strokeWidth = 5.dp)
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 16.dp),
+                color = Color.Red,
+                trackColor = Color.Green
+            )
+        }
+
+        Button(onClick = { showLoading = !showLoading }) {
+            Text(text = "Cargar Perfil")
+        }
+    }
+}
---
+++ Cursos/Jetpack compose/Componentes/ProgressBar/MyProgressBarAdvance.kt
@@ -0,0 +1,44 @@
+package com.mundocode.jetpackcomposecatalogo
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyProgessBarAdvanced() {
+
+    var progressStatus by rememberSaveable {
+        mutableStateOf(0f)
+    }
+
+    Column(Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        CircularProgressIndicator(progress = progressStatus)
+
+        Row(Modifier.fillMaxSize()) {
+            Button(onClick = { progressStatus += 0.1f }) {
+                Text(text = "Incrementar")
+            }
+            Button(onClick = { progressStatus -= 0.1f }) {
+                Text(text = "Reducir")
+            }
+        }
+    }
+}
