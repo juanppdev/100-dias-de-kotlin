e660a82c6ef611bbe9a7aa6bdea8b19b30673d07
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-04-18 21:43:32 +0200

    Login de Instagram en Jetpack Compose

---
+++ Proyectos/JetpackComposeInstagram/app/src/main/java/com/mundocode/jetpackcomposeinstagram/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.mundocode.jetpackcomposeinstagram
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mundocode.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeInstagramTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeInstagramTheme {
+        Greeting("Android")
+    }
+}
